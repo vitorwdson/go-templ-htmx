df1762cdd55ca73e8ebb68ac56f60828ce4b9665
Vitor Wdson <vitor.wdson2@gmail.com>
2023-12-17 14:41:23 -0400

    Added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './view/**/*.templ',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ]
+}
+
