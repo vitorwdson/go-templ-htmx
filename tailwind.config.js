532d6218838b749e9a5cc6a4b24c2f08c2bfb4db
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-11 20:30:29 -0400

    Reading tailwind classes from html and go files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './view/**/*.html',
     './view/**/*.templ',
+    './view/**/*.go',
   ],
   theme: {
     extend: {},
