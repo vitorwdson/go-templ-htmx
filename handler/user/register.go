faa9bfa05d1d59d87ec32beb5d78d483594dc401
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-14 17:07:05 -0400

    Saving user to DB and redirecting to profile page

--- handler/user/register.go
+++ handler/user/register.go
@@ -1,8 +1,6 @@
 package user
 
 import (
-	"fmt"
-
 	"github.com/labstack/echo/v4"
 	userModel "github.com/vitorwdson/go-templ-htmx/model/user"
 	"github.com/vitorwdson/go-templ-htmx/utils"
@@ -31,18 +29,24 @@ func (h UserHandler) PostRegister(c echo.Context) error {
 		return err
 	}
 
+	validationError := false
+
 	nameError := ""
 	if data.Name == "" {
 		nameError = "The user does not have a name"
+		validationError = true
 	}
 
 	usernameError := ""
 	if data.Username == "" {
 		usernameError = "The user does not have an username"
+		validationError = true
 	} else if len(data.Username) > 30 {
 		usernameError = "The username is too big (max: 30)"
+		validationError = true
 	} else if u, _ := userModel.GetByUsername(h.DB, data.Username); u != nil {
 		usernameError = "This username already exists."
+		validationError = true
 	}
 
 	passwordError := userModel.CheckPasswordStrength(data.Password)
@@ -50,11 +54,41 @@ func (h UserHandler) PostRegister(c echo.Context) error {
 		passwordError = "Both passwords must match."
 	}
 
+	if passwordError != "" {
+		validationError = true
+	}
+
 	emailError := ""
 	if data.Email == "" {
 		emailError = "The user does not have a email"
+		validationError = true
 	} else if len(data.Email) > 100 {
 		emailError = "The nickname is too big (max: 100)"
+		validationError = true
+	}
+
+	var user *userModel.User
+	if !validationError {
+		user = &userModel.User{
+			Name:     data.Name,
+			Username: data.Username,
+			Email:    data.Email,
+		}
+
+		err := user.SetPassword([]byte(data.Password))
+		if err != nil {
+			passwordError = err.Error()
+			user = nil
+		}
+	}
+
+	if user != nil {
+		err := user.Save(h.DB)
+		if err != nil {
+			return err
+		}
+
+		return utils.RedirectHtmx(c, "/profile")
 	}
 
 	props := userView.RegisterViewProps{
