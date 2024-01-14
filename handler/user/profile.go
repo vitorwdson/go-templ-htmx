425f03cedf21bb39d3bdc78d5872dcc3f6d67e3e
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-14 18:50:05 -0400

    Reading user data from session

--- handler/user/profile.go
+++ handler/user/profile.go
@@ -2,18 +2,15 @@ package user
 
 import (
 	"github.com/labstack/echo/v4"
-	userModel "github.com/vitorwdson/go-templ-htmx/model/user"
 	"github.com/vitorwdson/go-templ-htmx/utils"
 	userView "github.com/vitorwdson/go-templ-htmx/view/user"
 )
 
 func (h UserHandler) Profile(c echo.Context) error {
-	u := userModel.User{
-		Name: "Vitor Wdson",
-		Email: "vitor.wdson2@gmail.com",
+	session, err := GetSession(c, h.Redis)
+	if err != nil {
+		return utils.RedirectHtmx(c, "/login")
 	}
 
-	return utils.Render(c, userView.Profile(u))
+	return utils.Render(c, userView.Profile(session.User))
 }
-
-
