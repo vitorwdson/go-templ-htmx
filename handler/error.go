d30ea98862cf086d2875e386920968cce47226d6
Vitor Wdson <vitor.wdson2@gmail.com>
2024-01-28 15:09:28 -0400

    Default error handling

--- handler/error.go
+++ handler/error.go
@@ -1,6 +1,10 @@
 package handler
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/vitorwdson/go-templ-htmx/utils"
+)
 
 type ServerError struct {
 	Message    string
@@ -35,6 +39,18 @@ func (h handler) handleErrors(f RouteHandler) http.HandlerFunc {
 			return
 		}
 
-		// TODO: Handle errors here
+		serr, ok := err.(ServerError)
+		if !ok {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(err.Error()))
+		}
+
+		if serr == UserNotAuthenticated {
+			utils.RedirectHtmx(w, r, "/login")
+			return
+		}
+
+		w.WriteHeader(serr.StatusCode)
+		w.Write([]byte(serr.Message))
 	}
 }
